af3e224a9d7b5a8fa201c9bbd833dcb690ab4769
Simyumi <gkghgkgh0@naver.com>
2023-04-26 16:04:45 +0900

    counter만들기 완성

---
+++ 02.Counter/2-counter만들기.js
@@ -0,0 +1,33 @@
+const count = document.querySelector(".count_num");
+const decrease = document.querySelector(".decrease");
+const reset = document.querySelector(".reset");
+const increase = document.querySelector(".increase");
+
+decrease.addEventListener("click", function () {
+  let num = Number(count.textContent);
+  num = num - 1;
+  count.textContent = num;
+  colorChange();
+});
+increase.addEventListener("click", function () {
+  let num = Number(count.textContent);
+  num = num + 1;
+  count.textContent = num;
+  colorChange();
+});
+reset.addEventListener("click", function () {
+  let num = Number(count.textContent);
+  num = 0;
+  count.textContent = num;
+  colorChange();
+});
+
+function colorChange() {
+  if (Number(count.textContent) < 0) {
+    count.style.color = "red";
+  } else if (Number(count.textContent) == 0) {
+    count.style.color = "#333";
+  } else if (Number(count.textContent) > 0) {
+    count.style.color = "green";
+  }
+}
